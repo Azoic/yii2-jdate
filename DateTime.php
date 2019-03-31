0787ee01e1a54b1eef30fa7f809463c0b7c7d19d
Amin <a.dashti71@gmail.com>
2019-03-31 13:55:18 +0430

    Update DateTime.php

--- DateTime.php
+++ DateTime.php
@@ -9,7 +9,7 @@ use yii\base\InvalidParamException;
  * @author    Mohammad Mahdi Gholomian.
  * @copyright 2014 mm.gholamian@yahoo.com
  */
-class DateTime
+class DateTime extends \yii\base\Component
 {
     public function date($dateFormat, $timeStamp = false)
     {
@@ -136,7 +136,6 @@ class DateTime
 
     public function toJalaliDate($g_y, $g_m, $g_d)
     {
-
         $g_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
         $j_days_in_month = [31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29];
         $gy              = $g_y - 1600;
@@ -169,37 +168,6 @@ class DateTime
         $jd = $j_day_no + 1;
         return [$jy, $jm, $jd];
 
-    }
-
-    public function jalaliToGregorian($Date, $mod = '/')
-    {
-        list($jy, $jm, $jd) = explode('/', $Date);
-        if ($jy > 979) {
-            $gy = 1600;
-            $jy -= 979;
-        } else {
-            $gy = 621;
-        }
-        $days = (365 * $jy) + (((int)($jy / 33)) * 8) + ((int)((($jy % 33) + 3) / 4)) + 78 + $jd + (($jm < 7) ? ($jm - 1) * 31 : (($jm - 7) * 30) + 186);
-        $gy   += 400 * ((int)($days / 146097));
-        $days %= 146097;
-        if ($days > 36524) {
-            $gy   += 100 * ((int)(--$days / 36524));
-            $days %= 36524;
-            if ($days >= 365) $days++;
-        }
-        $gy   += 4 * ((int)($days / 1461));
-        $days %= 1461;
-        if ($days > 365) {
-            $gy   += (int)(($days - 1) / 365);
-            $days = ($days - 1) % 365;
-        }
-        $gd = $days + 1;
-        foreach ([0, 31, (($gy % 4 == 0 and $gy % 100 != 0) or ($gy % 400 == 0)) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] as $gm => $v) {
-            if ($gd <= $v) break;
-            $gd -= $v;
-        }
-        return ($mod == '') ? [$gy, $gm, $gd] : $gy . $mod . $gm . $mod . $gd;
     }
 
     public function div($a, $b)
